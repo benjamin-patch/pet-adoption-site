81ff5f72df98d1603c6d98a3f417a3f73720c87d
Benjamin Patch <ben@benjaminpatch.com>
2025-04-11 23:38:59 -0700

    Write some starter content.

--- app.py
+++ app.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '<h1>Adopt a Pet!</h1>'
+  return '''
+  <h1>Adopt a Pet!</h1>
+  <p>Browse through the links below to find your new furry friend:</p>
+  <ul>
+    <li>Dogs</li>
+    <li>Cats</li>
+    <li>Rabbits</li>
+  </ul>
+  '''
